81c641f501685a1d625aff5e4195a2648c42592d
lolass29 <lolass29@mail.ru>
2023-03-23 12:42:11 +0300

    1

---
+++ script.js
@@ -0,0 +1,109 @@
+//document.write("ggcgdc");
+//console.log("dgsg");
+//console.info("sgssgs");
+//console.error("cbdgg");
+//console.warn("dhdh0");
+var num = 5.5;
+//console.log("hfdhfdh:" + num + ".");
+const number = true;
+var num1 = 15;
+console.log("shsh:"+(num1-num));
+var num3 = 5;
+num3*=7;
+console.log(num3);
+var str_1 = Number("12");
+var str_2 = Number("2");
+console.log(str_1 + str_2);
+
+console.log("fhdhd:" + Math.PI);
+console.log(Math.min(0,25));
+
+var num123 = 15;
+var num321 = true;
+
+if(num123 <= 5 && num321 == true){ //|| или && и
+    console.log("Ok");
+}
+else if(num123==15){
+    console.log("Good");
+}
+else{
+    console.log("No");
+}
+
+var str_3 = "gjdjjji";
+
+switch(str_3){
+    case "4":
+        console.log("dhys");
+        break;
+    case "gjdjji":
+        console.log("bbsgs");
+        break;
+    default:
+        console.log("fdgdgd");
+
+}
+
+//for(var i = 0; i <= 10; i++){
+//    console.log(i);
+//}
+
+//var a = 0;
+//while(a < 10){
+  //  console.log(a);
+    //a++;
+//}
+
+var x = 100;
+do {
+    console.log(x);
+    x++;
+}
+while(x<50){
+    console.log(x);
+}
+
+for(var x=10; x<=20; x+=2){
+    if(x > 15){
+        break;
+    }
+    console.log(x);
+}
+
+for(var x = 10; x<=20; x++){
+    if (x % 2 == 0){
+        continue;
+    }
+    console.log(x);
+}
+
+var arr = [5, 7, 9, 3];
+
+for(var i = 0; i < arr.length; i++){ //length кол-во элементов в массиве (4)
+    console.log(i + " " + arr[i]);
+}
+
+//alert("fffyfyfy");
+
+//confirm("jjhvfhj");
+
+//var date = confirm("gfdsgsdg");
+//console.log(date);
+//if (data = true){
+//    alert("gdsgs");
+//}
+//else{
+//    alert("gsgsg");
+//}
+
+//prompt("fyfyf");
+
+//var lol = null;
+//if(confirm("SDfs")){
+ //   lol = prompt("dhdh");
+  //  alert("sgsgsg, " + lol);
+//}
+//else{
+  //  alert("ddgdgd");
+//}
